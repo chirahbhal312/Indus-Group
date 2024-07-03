31d99080358778bdc333944680e20a962802d6a5
chirahbhal312 <chiragcodes0@gmail.com>
2024-07-03 17:21:11 +0530

    Frontend Structure

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button><Link to="/page1">Go to Page 1</Link></button>
+      <button><Link to="/page2">Go to Page 2</Link></button>
+      <button><Link to="/page3">Go to Page 3</Link></button>
+    </div>
+  );
+}
+
+export default HomePage;
